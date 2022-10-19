5d7f0e32b237d9b1ea2d9ce70097398f98debfdd
Khanim Humbatova <khumbatova@pgh.yearup.org>
2022-10-19 13:08:25 -0400

    added if else

---
+++ pay_rules.js
@@ -0,0 +1,21 @@
+/*var payRate = 10.50;
+var hoursWorked = 20;
+var hoursSupposetoWork = 40;
+var grossPay = payRate * hoursWorked;
+ console.log(grossPay);
+
+var payRate = 15.00;
+var hoursWorked = 45;
+var hoursSupposetoWork = 40;
+var overTime = hoursWorked - hoursSupposetoWork;
+console.log(overTime); 
+var overTimePay = payRate * overTime * 1.5;
+console.log(overTimePay);
+var grossPay = (payRate * hoursSupposetoWork) + overTimePay;
+console.log(grossPay);*/
+
+var payRate = 20.00;
+var hoursWorked = 45;
+if (hoursWorked > 40) {
+   console.log()
+}
